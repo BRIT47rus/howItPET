b97185e35a0832a14d534c41a7e0700edba189a0
Vladimir Noutbook <vovirus2@inbox.ru>
2025-04-17 15:43:16 +0300

    save

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,9 +1,15 @@
-import { useTheme } from '../../app/providers/contexts/ThemeContext';
+import { useEffect } from 'react';
+import { THEME, useTheme } from '../../app/providers/contexts/ThemeContext';
 import { Button, LinkHref } from '../../shared';
 import cls from './Header.module.scss';
 
 export const Header = () => {
-    const { changeTheme } = useTheme();
+    const { changeTheme, theme } = useTheme();
+    const isLight = theme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT;
+    useEffect(() => {
+        document.body.setAttribute('data-theme', isLight);
+    }, [theme]);
+
     return (
         <div className={cls.header}>
             <LinkHref label="Главная" />
