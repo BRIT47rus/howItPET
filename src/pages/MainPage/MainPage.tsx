82642aefb8d0bef0faa6e11eadae8d791e54b49d
Vladimir Noutbook <vovirus2@inbox.ru>
2025-04-28 16:42:37 +0300

    убрал стате и юзкэфект

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,41 +1,37 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/providers/store/store';
 import { Container } from '../../widgets';
-import { useState, useEffect } from 'react'; // Импортируйте ArticleT
+import { useState } from 'react'; // Импортируйте ArticleT
 import { ArticleT } from '../../app/db/howDates/types';
 import cls from './MainPage.module.scss';
 import { Article } from '../../widgets/Article/Article';
 
 export const MainPage = () => {
-    // Получаем состояние 'article' из Redux с помощью useSelector
     const articleState: ArticleT = useSelector(
         (state: RootState) => state.article
     );
-    const [dataStore, setDataStore] = useState<ArticleT | null>(articleState);
-    const [open, setOpen] = useState(false);
+    const [openArticle, setOpenArticle] = useState(false);
 
     const handleClick = () => {
-        setOpen((prev) => !prev);
+        setOpenArticle((prev) => !prev);
     };
-
-    // Эффект, который следит за изменениями в articleState и обновляет локальное состояние
-    useEffect(() => {
-        setDataStore(articleState);
-    }, [articleState]); // Зависимость - значение, полученное из useSelector
-    console.log(dataStore);
+    console.log('articleState:', articleState);
     return (
         <Container classess={cls.content}>
-            <h1>{dataStore?.titleData || 'Нет данных'}</h1>
-            {dataStore &&
-                dataStore.info.map((item) => (
+            <h1>{articleState?.titleData || 'Нет данных'}</h1>
+            {articleState?.info?.map(
+                (
+                    item // Добавлена безопасная навигация
+                ) => (
                     <Article
                         key={item.id}
                         item={item}
                         label={item.title}
-                        open={open}
+                        open={openArticle}
                         handleClick={handleClick}
                     />
-                ))}
+                )
+            )}
         </Container>
     );
 };
